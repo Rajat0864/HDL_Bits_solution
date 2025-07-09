b48cbf4e479031d8b88e37b93082f9316f59b5cf
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-07-09 22:16:07 +0530

    Create Simple_FSM2(async).v

---
+++ FSM/Simple_FSM2(async).v
@@ -0,0 +1,33 @@
+//always try to figure out what is happening during the state transistion . everything is same a 1st fsm question but this time we have 2 inputs.
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        case(state)
+            OFF: next_state = j ?ON : OFF;
+            ON: next_state = k ? OFF : ON;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+        if(areset)begin
+            state <= OFF;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+    // Output logic
+    assign out = (state == ON);
+
+endmodule
