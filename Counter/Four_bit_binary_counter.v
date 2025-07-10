45ddddde80150b51fac6f6c328e4ae309adf799a
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-07-10 11:40:10 +0530

    Update Four_bit_binary_counter.v

--- Counter/Four_bit_binary_counter.v
+++ Counter/Four_bit_binary_counter.v
@@ -4,7 +4,7 @@ module top_module (
     output [3:0] q);
     always@(posedge clk)begin
         if(reset) begin
-            q<='0;
+            q<='0; //('0 means set all bits of q to zero)
         end
         else begin
             q<=q+1;
