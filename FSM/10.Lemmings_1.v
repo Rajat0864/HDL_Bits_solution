84a7b75b81dd8ca6bcbae25c6c2a050dab9ce690
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-07-20 12:33:19 +0530

    Create 10.Lemmings_1.v

---
+++ FSM/10.Lemmings_1.v
@@ -0,0 +1,36 @@
+//lemmings 1
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    output walk_left,
+    output walk_right); //  
+
+    // parameter LEFT=0, RIGHT=1, ...
+    reg state, next_state;
+    parameter LEFT = 0 ,RIGHT =1;
+
+    always @(*) begin
+        // State transition logic
+        case(state)
+            LEFT:next_state = bump_left ? RIGHT:LEFT;
+             RIGHT:next_state = bump_right ? LEFT:RIGHT;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if(areset) begin
+            state <=LEFT;
+            
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+    // Output logic
+    assign walk_right = (state ==RIGHT);
+    assign walk_left = (state == LEFT);
+
+endmodule
