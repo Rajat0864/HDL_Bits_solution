21022b91092aed9b1f8e5c09a0fce67539a8207d
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-08-15 23:14:12 +0530

    Update 4.Lfsr5.v

--- Shift_registers/4.Lfsr5.v
+++ Shift_registers/4.Lfsr5.v
@@ -1,1 +1,19 @@
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 5'h1
+    output [4:0] q
+); 
+    reg g1 ;
+    
+    always @ ( posedge clk ) begin
+        
+        if (reset ) begin
+            q <= 4'd1 ;
+        end
+        else begin
+            g1 = q[3] ^ q[0] ;
+            q <= {q[0],q[4],g1,q[2],q[1]};
+        end
+    end
+endmodule
 
