5f22ec6b142bc7fcf3c1d341a070561527640603
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-08-18 10:39:28 +0530

    Create 6.shift_register.v

---
+++ Shift_registers/6.shift_register.v
@@ -0,0 +1,23 @@
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+    reg q1,q2,q3 ;
+    always @ (posedge clk  ) begin
+        if ( !resetn ) begin
+            out <= 0 ;
+            q1 <= 0;
+            q2 <=0 ;
+            q3 <= 0;
+        end
+        else begin
+            q1 <= in;
+             q2 <=q1;
+             q3 <= q2;
+             out <= q3;
+        end
+    end
+            
+
+endmodule
