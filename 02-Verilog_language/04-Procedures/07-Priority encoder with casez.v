3daf525231778051c446ceb06de5dc0c4da836c5
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-08-25 23:00:01 +0530

    Update 07-Priority encoder with casez.v

--- 02-Verilog_language/04-Procedures/07-Priority encoder with casez.v
+++ 02-Verilog_language/04-Procedures/07-Priority encoder with casez.v
@@ -1,10 +1,10 @@
 // synthesis verilog_input_version verilog_2001
 module top_module (
     input [7:0] in,
-    output reg [2:0] pos );
-  
-    always @ ( * ) begin
-        casez ( in )
+    output reg [2:0] pos
+);
+    always @(*) begin
+        casez (in)
             8'b???????1 : pos = 3'b000;
             8'b??????10 : pos = 3'b001;
             8'b?????100 : pos = 3'b010;
@@ -13,9 +13,7 @@ module top_module (
             8'b??100000 : pos = 3'b101;
             8'b?1000000 : pos = 3'b110;
             8'b10000000 : pos = 3'b111;
-            default : pos = 0;
+            default     : pos = 3'b000; 
         endcase
     end
-
 endmodule
-
