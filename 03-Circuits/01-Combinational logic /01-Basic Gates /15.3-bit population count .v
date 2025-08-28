3a22efe5da1af648d73190d24eff9cf89665068f
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-08-28 16:24:58 +0530

    Create 15.3-bit population count .v

---
+++ 03-Circuits/01-Combinational logic /01-Basic Gates /15.3-bit population count .v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+    integer i ;
+    always @  ( * ) begin
+        out = 0 ;
+        for ( i = 0 ; i <= 2 ; i++ )begin
+            out = out + in[i] ;
+        end
+    end
+
+endmodule
