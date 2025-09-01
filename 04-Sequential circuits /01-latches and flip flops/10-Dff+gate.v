920f847f32373f173cdacc7f2314084c38db4a81
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-09-01 18:09:48 +0530

    Create 10-Dff+gate.v

---
+++ 04-Sequential circuits /01-latches and flip flops/10-Dff+gate.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input in, 
+    output out);
+    reg d ;
+    assign d = in ^ out ;
+    always @ ( posedge clk ) begin
+        out <= d ;
+    end
+      
+
+endmodule
