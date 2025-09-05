9a8a5ec74e6e48186a549aea8678267e8b1a7fa1
Rajat0864 <157405666+Rajat0864@users.noreply.github.com>
2025-09-05 22:37:37 +0530

    Create 21.Serial_two's_complement(Moore_fsm).v

---
+++ FSM/21.Serial_two's_complement(Moore_fsm).v
@@ -0,0 +1,24 @@
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+    parameter A = 0, B = 1, C = 2 ;
+    reg [1:0] state, next_state ;
+    always @ ( posedge clk or posedge areset) begin
+        if ( areset ) 
+        state <= A ;
+        else 
+            state <= next_state ;
+    end
+    always @ ( * ) begin
+        case ( state ) 
+            A : next_state = x ? B : A ;
+            B : next_state = x ? C : B ;
+            C : next_state = x ? C : B ;
+            default : next_state = A ;
+        endcase
+    end
+    assign z = state[0] ;
+endmodule
